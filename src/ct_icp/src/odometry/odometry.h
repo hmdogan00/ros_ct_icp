a2ca10ea1f8fe673a1d5405af949bca3772816a9
hmdogan00 <hamimert@outlook.com>
2022-06-27 09:39:08 +0300

    initial commit

---
+++ src/ct_icp/src/odometry/odometry.h
@@ -0,0 +1,301 @@
+#ifndef CT_ICP_ODOMETRY_H
+#define CT_ICP_ODOMETRY_H
+
+#include <map>
+#include "../cost_functions/cost_func.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <list>
+
+#define _USE_MATH_DEFINES
+
+#include <fstream>
+#include <vector>
+#include <random>
+#include <unordered_map>
+
+#include <Eigen/Dense>
+
+#include "../utils/types.h"
+
+namespace ct_icp {
+
+  enum MOTION_COMPENSATION {
+    NONE = 0,              // No compensation of the motion
+    CONSTANT_VELOCITY = 1, // Initial distortion of the point cloud based on the estimated velocity
+    ITERATIVE = 2,         // Iterative refinement after each ICP iteration
+    CONTINUOUS = 3         // Continuous estimation of the pose
+  };
+
+  enum INITIALIZATION {
+    INIT_NONE = 0,
+    INIT_CONSTANT_VELOCITY = 1
+  };
+
+  enum CT_ICP_SOLVER {
+    GN = 0,
+    CERES = 1
+  };
+
+  enum LEAST_SQUARES {
+    STANDARD = 0,
+    CAUCHY = 1,
+    HUBER = 2,
+    TOLERANT = 3,
+    TRUNCATED = 4
+  };
+
+  enum WEIGHTING_SCHEME {
+    PLANARITY ,      // Weighs residuals by their planarity coefficient
+    NEIGHBORHOOD ,   // Weighs residuals by the distance to their neares neighbors
+    ALL          // Combines all weighting schemes with different coefficients
+  };
+
+  struct CTICPOptions {
+
+    // The threshold on the voxel occupancy
+    // To be considered in the neighbor search, a voxel must have at least threshold_voxel_occupancy points
+    int threshold_voxel_occupancy = 1;
+
+    int init_num_frames = 20; // The number of frames defining the initialization of the map
+
+    double size_voxel_map = 1.0; //Max Voxel : -32767 to 32767 then 32km map for SIZE_VOXEL_MAP = 1m
+
+    int num_iters_icp = 5; // The Maximum number of ICP iterations performed
+
+    int min_number_neighbors = 20;
+
+    int voxel_neighborhood = 1; // Visits the (3 * voxel_neighborhood)^3 neighboring voxels
+
+    double power_planarity = 2.0; // The power of planarity defined in the weighting scheme
+
+    // Whether to estimate the normal of the key point or the closest neighbor
+    bool estimate_normal_from_neighborhood = true;
+
+    int max_number_neighbors = 20;
+
+    double max_dist_to_plane_ct_icp = 0.3; // The maximum distance point-to-plane (OLD Version of ICP)
+
+    double threshold_orientation_norm = 0.0001; // Threshold on rotation (deg) for ICP's stopping criterion
+
+    double threshold_translation_norm = 0.001; // Threshold on translation (deg) for ICP's stopping criterion
+
+    bool point_to_plane_with_distortion = true; // Whether to distort the frames at each ICP iteration
+
+    int max_num_residuals = -1; // The maximum number of keypoints used
+
+    int min_num_residuals = 100; // Below this number, CT_ICP will crash
+
+    ICP_DISTANCE distance = CT_POINT_TO_PLANE;
+
+    int num_closest_neighbors = 1; // The number of closest neighbors considered as residuals
+
+    // TODO : Add Trajectory Constraints Options
+    double beta_location_consistency = 0.001; // Constraints on location
+
+    double beta_constant_velocity = 0.001; // Constraint on velocity
+
+    double beta_small_velocity = 0.0; // Constraint on the relative motion
+
+    double beta_orientation_consistency = 0.0; // Constraint on the orientation consistency
+
+    WEIGHTING_SCHEME weighting_scheme = ALL;
+
+    double weight_alpha = 0.9;
+
+    double weight_neighborhood = 0.1;
+
+    CT_ICP_SOLVER solver = GN;
+
+    /* ---------------------------------------------------------------------------------------------------------- */
+    /* LEAST SQUARE OPTIMIZATION PARAMETERS                                                                       */
+
+    LEAST_SQUARES loss_function = CAUCHY;
+
+    int ls_max_num_iters = 1;
+
+    int ls_num_threads = 16;
+
+    double ls_sigma = 0.1; // The robust parameter (for Cauchy, Huber or truncated least square)
+
+    double ls_tolerant_min_threshold = 0.05; // The Tolerant
+
+    // Debug params
+    bool debug_print = true; // Whether to output debug information to std::cout
+
+  };
+
+  struct OdometryOptions {
+
+    /* Parameters for initialization of the map */
+    double init_voxel_size = 0.2;
+
+    double init_sample_voxel_size = 1.0;
+
+    int init_num_frames = 20; // The number of frames defining the initialization of the map
+
+    double voxel_size = 0.5;
+
+    double sample_voxel_size = 1.5;
+
+    double max_distance = 100.0; // The threshold on the voxel size to remove points from the map
+
+    int max_num_points_in_voxel = 20; // The maximum number of points in a voxel
+
+    double min_distance_points = 0.1; // The minimal distance between points in the map
+
+    double distance_error_threshold = 5.0; // The Ego-Motion Distance considered as an error
+
+    // Whether to assess the quality of the registration,
+    // And try a new registration with more conservative parameters in case of failure
+    int robust_minimal_level = 0;
+    bool robust_registration = false;
+    double robust_full_voxel_threshold = 0.7;
+    double robust_empty_voxel_threshold = 0.1;
+    double robust_neighborhood_min_dist = 0.10; // The minimum relative distance to launch a robust neighborhood test
+    double robust_neighborhood_min_orientation = 0.1; // The minimum relative orientation to launch a robust neighborhood
+    // Threshold on the relative transform (all motion at 10Hz should be below this value)
+    double robust_relative_trans_threshold = 1.0;
+    bool robust_fail_early = false; // Stop iterations if the final assessment of the registration is unsucessful
+    int robust_num_attempts = 6;
+    int robust_num_attempts_when_rotation = 2;
+    int robust_max_voxel_neighborhood = 3;
+    double robust_threshold_ego_orientation = 3; // Angle in degrees
+    double robust_threshold_relative_orientation = 3; // Angle in degrees
+
+    CTICPOptions ct_icp_options;
+
+    MOTION_COMPENSATION motion_compensation = CONTINUOUS;
+
+    INITIALIZATION initialization = INIT_CONSTANT_VELOCITY;
+
+
+    // Debug Parameters
+    bool debug_print = true; // Whether to print debug information into the console
+
+    bool debug_viz = false; // Whether to display the Local Map in a window
+
+    bool log_to_file = false;
+
+    std::string log_file_destination = "/tmp/ct_icp.log";
+  };
+
+  // Add Points To the Map
+  void AddPointsToMap(VoxelHashMap& map, const std::vector<Point3D>& points,
+    double voxel_size, int max_num_points_in_voxel,
+    double min_distance_points, int min_num_points = 0);
+
+  // Add Points To the Map
+  void AddPointsToMap(VoxelHashMap& map, const ArrayVector3d& points, double voxel_size,
+    int max_num_points_in_voxel, double min_distance_points);
+
+  // Remove voxels far from the given location
+  void RemovePointsFarFromLocation(VoxelHashMap& map, const Eigen::Vector3d& location, double distance);
+
+  // Extracts points of the local map into a PointCloud
+  ArrayVector3d MapAsPointcloud(const VoxelHashMap& map);
+
+  // Compute the size of a VoxelHashMap
+  size_t MapSize(const VoxelHashMap& map);
+
+
+  class Odometry {
+    public:
+
+    // The Output of a registration, including metrics,
+    struct RegistrationSummary {
+
+      TrajectoryFrame frame;
+
+      int sample_size = 0; // The number of points sampled
+
+      int number_of_residuals = 0; // The number of keypoints used for ICP registration
+
+      int robust_level = 0;
+
+      double distance_correction = 0.0; // The correction between the last frame's end, and the new frame's beginning
+
+      double relative_distance = 0.0; // The distance between the beginning of the new frame and the end
+
+      double relative_orientation = 0.0; // The distance between the beginning of the new frame and the end
+
+      double ego_orientation = 0.0; // The angular distance between the beginning and the end of the frame
+
+      bool success = true; // Whether the registration was a success
+
+      int number_of_attempts = 0; // The number of attempts at registering the new frame
+
+      std::string error_message;
+
+      std::vector<Point3D> corrected_points; // Sampled points expressed in the initial frame
+
+      std::vector<Point3D> all_corrected_points; // Initial points expressed in the initial frame
+
+      std::vector<Point3D> keypoints; // Last Keypoints selected
+
+    };
+
+    explicit Odometry(const OdometryOptions& options);
+
+    explicit Odometry(const OdometryOptions* options) : Odometry(*options) {}
+
+    // Registers a new Frame to the Map
+    RegistrationSummary RegisterFrame(const std::vector<Point3D>& frame);
+
+    // Registers a new Frame to the Map with an initial estimate
+    RegistrationSummary RegisterFrameWithEstimate(const std::vector<Point3D>& frame,
+      const TrajectoryFrame& initial_estimate);
+
+    // Returns the currently registered trajectory
+    [[nodiscard]] std::vector<TrajectoryFrame> Trajectory() const;
+
+    // Returns the Aggregated PointCloud of the Local Map
+    [[nodiscard]] ArrayVector3d GetLocalMap() const;
+
+    // Num Points in the Map
+    // Note: This requires a traversal of the whole map which is in O(n)
+    [[nodiscard]] size_t MapSize() const;
+
+    private:
+    std::vector<TrajectoryFrame> trajectory_;
+    VoxelHashMap voxel_map_;
+    int registered_frames_ = 0;
+    int robust_num_consecutive_failures_ = 0;
+    bool suspect_registration_error_ = false;
+    int next_robust_level_ = 0;
+    OdometryOptions options_;
+    std::ostream* log_out_ = nullptr;
+    std::unique_ptr<std::ofstream> log_file_ = nullptr;
+
+    // Initialize the Frame
+    std::vector<Point3D> InitializeFrame(const std::vector<Point3D>& const_frame,
+      int index_frame);
+
+    // Registers a frame after the motion was initialized
+    // When the Robust Registration profile is activated, it can call TryRegister
+    // Multiple times changing the options in order to increase the chance of registration
+    RegistrationSummary DoRegister(const std::vector<Point3D>& frame, int frame_index);
+
+    // Tries to register a frame given a set of options
+    RegistrationSummary TryRegister(std::vector<Point3D>& frame,
+      int frame_index,
+      const CTICPOptions& options,
+      RegistrationSummary& registration_summary,
+      double sample_voxel_size);
+
+    // Insert a New Trajectory Frame, and initializes the motion for this new frame
+    int InitializeMotion(const TrajectoryFrame* initial_estimate = nullptr);
+
+    // Try to insert Points to the map
+    // Returns false if it fails
+    bool AssessRegistration(const std::vector<Point3D>& points, RegistrationSummary& summary,
+      std::ostream* log_stream = nullptr) const;
+
+  };
+
+} // namespace ct_icp
+
+#endif
---
+++ src/ct_icp/src/slam.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+#include <mutex>
+#include <thread>
+
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/PCLPointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include "odometry/odometry.h"
+
+int init, motion, dist, ls, solver, weight;
+std::mutex registration_mutex;
+const std::string main_frame_id = "/Odometry";
+const std::string child_frame_id = "/child";
+
+struct Options{
+    ct_icp::OdometryOptions odometry_options;
+    std::string lidar_topic = "/center/pandar";
+    int max_num_threads = 1; // The maximum number of threads running in parallel the Dataset acquisition
+    bool suspend_on_failure = false; // Whether to suspend the execution once an error is detected
+    bool save_trajectory = true; // whether to save the trajectory
+    std::string output_dir = "./outputs"; // The output path (relative or absolute) to save the pointclouds
+    std::string sequence; // The desired sequence (only applicable if `all_sequences` is false)
+    int start_index = 0; // The start index of the sequence (only applicable if `all_sequences` is false)
+    int max_frames = -1; // The maximum number of frames to register (if -1 all frames in the Dataset are registered)
+};
+
+Options get_options(ros::NodeHandle nh) {
+    Options options;
+    nh.param<int>("/max_num_threads", options.max_num_threads, 1);
+    nh.param<std::string>("/output_dir", options.output_dir, ".outputs");
+    nh.param<std::string>("/lidar_topic", options.lidar_topic, "/center/pandar");
+    nh.param<bool>("/save_trajectory", options.save_trajectory, true);
+    nh.param<bool>("/suspend_on_failure", options.suspend_on_failure, false);
+
+    nh.param<bool>("/odometry_options/debug_print", options.odometry_options.debug_print, true);
+    nh.param<double>("/odometry_options/distance_error_threshold", options.odometry_options.distance_error_threshold, 5.0);
+    nh.param<int>("/odometry_options/init_num_frames", options.odometry_options.init_num_frames, 20);
+    nh.param<int>("/odometry_options/initialization", init, 1);
+    options.odometry_options.initialization = static_cast<ct_icp::INITIALIZATION>(init);
+    nh.param<bool>("/odometry_options/log_to_file", options.odometry_options.log_to_file, false);
+    nh.param<double>("/odometry_options/max_distance", options.odometry_options.max_distance, 100.0);
+    nh.param<int>("/odometry_options/max_num_points_in_voxel", options.odometry_options.max_num_points_in_voxel, 20);
+    nh.param<double>("/odometry_options/min_distance_points", options.odometry_options.min_distance_points, 0.1);
+    nh.param<int>("/odometry_options/motion_compensation", motion, 3);
+    options.odometry_options.motion_compensation = static_cast<ct_icp::MOTION_COMPENSATION>(motion);
+    nh.param<double>("/odometry_options/robust_full_voxel_threshold", options.odometry_options.robust_full_voxel_threshold, 0.7);
+    nh.param<int>("/odometry_options/robust_max_voxel_neighborhood", options.odometry_options.robust_max_voxel_neighborhood, 4);
+    nh.param<int>("/odometry_options/robust_minimal_level", options.odometry_options.robust_minimal_level, 0);
+    nh.param<int>("/odometry_options/robust_num_attempts", options.odometry_options.robust_num_attempts, 6);
+    nh.param<bool>("/odometry_options/robust_registration", options.odometry_options.robust_registration, false);
+    nh.param<double>("/odometry_options/robust_threshold_ego_orientation", options.odometry_options.robust_threshold_ego_orientation, 2.0);
+    nh.param<double>("/odometry_options/robust_threshold_relative_orientation", options.odometry_options.robust_threshold_relative_orientation, 2.0);
+    nh.param<double>("/odometry_options/sample_voxel_size", options.odometry_options.sample_voxel_size, 1.5);
+    nh.param<double>("/odometry_options/voxel_size", options.odometry_options.voxel_size, 0.5);
+
+    nh.param<double>("/odometry_options/ct_icp_options/beta_constant_velocity", options.odometry_options.ct_icp_options.beta_constant_velocity, 0.001);
+    nh.param<double>("/odometry_options/ct_icp_options/beta_location_consistency", options.odometry_options.ct_icp_options.beta_location_consistency, 0.001);
+    nh.param<double>("/odometry_options/ct_icp_options/beta_orientation_consistency", options.odometry_options.ct_icp_options.beta_orientation_consistency, 0.0);
+    nh.param<double>("/odometry_options/ct_icp_options/beta_small_velocity", options.odometry_options.ct_icp_options.beta_small_velocity, 0.0);
+    nh.param<bool>("/odometry_options/ct_icp_options/debug_print", options.odometry_options.ct_icp_options.debug_print, false);
+    nh.param<int>("/odometry_options/ct_icp_options/distance", dist, 1);
+    options.odometry_options.ct_icp_options.distance = static_cast<ct_icp::ICP_DISTANCE>(dist);
+    nh.param<int>("/odometry_options/ct_icp_options/loss_function", ls, 1);
+    options.odometry_options.ct_icp_options.loss_function = static_cast<ct_icp::LEAST_SQUARES>(ls);
+    nh.param<int>("/odometry_options/ct_icp_options/ls_max_num_iters", options.odometry_options.ct_icp_options.ls_max_num_iters, 10);
+    nh.param<int>("/odometry_options/ct_icp_options/ls_num_threads", options.odometry_options.ct_icp_options.ls_num_threads, 8);
+    nh.param<double>("/odometry_options/ct_icp_options/ls_sigma", options.odometry_options.ct_icp_options.ls_sigma, 0.1);
+    nh.param<double>("/odometry_options/ct_icp_options/ls_tolerant_min_threshold", options.odometry_options.ct_icp_options.ls_tolerant_min_threshold, 0.05);
+    nh.param<double>("/odometry_options/ct_icp_options/max_dist_to_plane_ct_icp", options.odometry_options.ct_icp_options.max_dist_to_plane_ct_icp, 0.05);
+    nh.param<int>("/odometry_options/ct_icp_options/max_num_residuals", options.odometry_options.ct_icp_options.max_num_residuals, 1000);
+    nh.param<int>("/odometry_options/ct_icp_options/max_number_neighbors", options.odometry_options.ct_icp_options.max_number_neighbors, 20);
+    nh.param<int>("/odometry_options/ct_icp_options/min_num_residuals", options.odometry_options.ct_icp_options.min_num_residuals, 200);
+    nh.param<int>("/odometry_options/ct_icp_options/min_number_neighbors", options.odometry_options.ct_icp_options.min_number_neighbors, 20);
+    nh.param<int>("/odometry_options/ct_icp_options/num_closest_neighbors", options.odometry_options.ct_icp_options.num_closest_neighbors, 1);
+    nh.param<int>("/odometry_options/ct_icp_options/num_iters_icp", options.odometry_options.ct_icp_options.num_iters_icp, 30);
+    nh.param<bool>("/odometry_options/ct_icp_options/point_to_plane_with_distortion", options.odometry_options.ct_icp_options.point_to_plane_with_distortion, true);
+    nh.param<double>("/odometry_options/ct_icp_options/power_planarity", options.odometry_options.ct_icp_options.power_planarity, 2.0);
+    nh.param<double>("/odometry_options/ct_icp_options/size_voxel_map", options.odometry_options.ct_icp_options.size_voxel_map, 1.0);
+    nh.param<int>("/odometry_options/ct_icp_options/solver", solver, 1);
+    options.odometry_options.ct_icp_options.solver = static_cast<ct_icp::CT_ICP_SOLVER>(solver);
+    nh.param<double>("/odometry_options/ct_icp_options/threshold_orientation_norm", options.odometry_options.ct_icp_options.threshold_orientation_norm, 0.05);
+    nh.param<double>("/odometry_options/ct_icp_options/threshold_translation_norm", options.odometry_options.ct_icp_options.threshold_translation_norm, 0.005);
+    nh.param<int>("/odometry_options/ct_icp_options/threshold_voxel_occupancy", options.odometry_options.ct_icp_options.threshold_voxel_occupancy, 5);
+    nh.param<int>("/odometry_options/ct_icp_options/voxel_neighborhood", options.odometry_options.ct_icp_options.voxel_neighborhood, 1);
+    nh.param<double>("/odometry_options/ct_icp_options/weight_alpha", options.odometry_options.ct_icp_options.weight_alpha, 0.9);
+    nh.param<double>("/odometry_options/ct_icp_options/weight_neighborhood", options.odometry_options.ct_icp_options.weight_neighborhood, 0.1);
+    nh.param<int>("/odometry_options/ct_icp_options/weighting_scheme", weight, 2);
+    options.odometry_options.ct_icp_options.weighting_scheme = static_cast<ct_icp::WEIGHTING_SCHEME>(weight);
+
+    return options;
+}
+
+void pcl_cb(const sensor_msgs::PointCloud2::ConstPtr& input) {
+    std::lock_guard<std::mutex> guard{registration_mutex};
+
+    pcl::PointCloud<pandar_ros::Point> pcl_pc2;
+    pcl::fromROSMsg(*input, pcl_pc2);
+    std::cout << pcl_pc2 << std::endl;
+    auto stamp = pcl_pc2.header.stamp;
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "ct_icp");
+    ros::NodeHandle nh;
+
+    // Parameter registration
+    Options options = get_options(nh);
+
+    ros::Subscriber sub_pcl = nh.subscribe(options.lidar_topic, 200000, pcl_cb);
+    while (ros::ok()) {
+        ros::spinOnce();
+    }
+
+    return 0;
+}
